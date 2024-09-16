fa077fe8d747640fd8df3d5543bc9a6e63740938
Michael <mikefang456@gmail.com>
2024-09-16 00:32:13 -0700

    Unit 1 TF weekly task - Complete

---
+++ Template/unit#_session#.py
@@ -0,0 +1,74 @@
+# PROBLEM 1
+
+# 1. Share 2 questions you would ask to help understand the question:
+
+# 2. Write out in plain English what you want to do:
+
+# 3. Translate each sub-problem into pseudocode:
+
+# 4. Translate the pseudocode into Python and share your final answer:
+
+# Time Complexity:
+# Space Complexity:
+
+
+
+# PROBLEM 2
+
+# 1. Share 2 questions you would ask to help understand the question:
+
+# 2. Write out in plain English what you want to do:
+
+# 3. Translate each sub-problem into pseudocode:
+
+# 4. Translate the pseudocode into Python and share your final answer:
+
+# Time Complexity:
+# Space Complexity:
+
+
+
+# PROBLEM 3
+
+# 1. Share 2 questions you would ask to help understand the question:
+
+# 2. Write out in plain English what you want to do:
+
+# 3. Translate each sub-problem into pseudocode:
+
+# 4. Translate the pseudocode into Python and share your final answer:
+
+# Time Complexity:
+# Space Complexity:
+
+
+
+# PROBLEM 4
+
+# 1. Share 2 questions you would ask to help understand the question:
+
+# 2. Write out in plain English what you want to do:
+
+# 3. Translate each sub-problem into pseudocode:
+
+# 4. Translate the pseudocode into Python and share your final answer:
+
+# Time Complexity:
+# Space Complexity:
+
+
+
+# PROBLEM 5
+
+# 1. Share 2 questions you would ask to help understand the question:
+
+# 2. Write out in plain English what you want to do:
+
+# 3. Translate each sub-problem into pseudocode:
+
+# 4. Translate the pseudocode into Python and share your final answer:
+
+# Time Complexity:
+# Space Complexity:
+
+
