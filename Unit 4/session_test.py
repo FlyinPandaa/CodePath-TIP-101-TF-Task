b5700867ef5f4e9caf09b25bba8afce60b2dc63f
FlyinPandaa <mikefang456@gmail.com>
2024-10-10 19:03:20 -0700

    Updated session_test file for Unit 4

--- Unit 4/session_test.py
+++ Unit 4/session_test.py
@@ -9,3 +9,23 @@
 
 
 #Implement
+
+def reverse_list(lst):
+  left = 0
+  right = len(lst) - 1
+
+  while left < right:
+    # Line 19 - 22 // Swap
+    temp = lst[left]
+    lst[left] = lst[right]
+    lst[right] = temp
+
+    left += 1
+    right -= 1
+
+  return lst
+  pass
+
+lst = [1, 2, 3, 4, 5]
+print(lst)
+print(reverse_list(lst))
