fe457581bda785dffc210f5822f6c4d749b2d5a6
FlyinPandaa <mikefang456@gmail.com>
2024-10-17 19:15:27 -0700

    Minor addition explaining pointers in Unit 4

--- Unit 4/session_test.py
+++ Unit 4/session_test.py
@@ -45,3 +45,35 @@ def is_prime(n):
 print(is_prime(5))
 print(is_prime(12))
 print(is_prime(9))
+
+
+
+
+
+
+def modify_list(lst):
+    lst.append(100)
+
+my_list = [1, 2, 3]
+modify_list(my_list)
+print(my_list)  # Output: [1, 2, 3, 100]
+
+
+def modify_number(n):
+    n += 1
+
+my_number = 10
+modify_number(my_number)
+print(my_number)  # Output: 10
+
+
+def modify_value(pointer):
+    pointer[0] += 1  # Modify the first element
+
+my_value = [10]  # A list that contains an integer
+modify_value(my_value)
+print(my_value[0])  # Output: 11
+
+
+i = 0
+j = len(my_value) - 1
